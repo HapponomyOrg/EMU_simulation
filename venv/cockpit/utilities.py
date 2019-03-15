2c8fa0282b4bdaaa2e5ca0068879004e8f5ce988
Sven Van Echelpoel <sven.v.echelpoel@gmail.com>
2019-03-15 12:39:50 +0100

    deleted venv

--- venv/cockpit/utilities.py
+++
@@ -1,8 +0,0 @@
-# module utilities
-
-import pygal
-
-def create_chart(title):
-    chart = pygal.Line(show_dots=True)
-    chart.title = title
-    return chart
--- venv/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'kjbfjehbrf ezhb 3ihdf'
-    MONGO_URI = "mongodb://localhost:27017/myDatabase"
