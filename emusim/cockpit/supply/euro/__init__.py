e9b463243c4a87e54677ed77f0ee6dae68c650e3
stef <stef@thestandupway.org>
2020-08-29 13:39:15 +0200

    Removed blank lines
    Signed-off-by: stef <stef@thestandupway.org>

--- emusim/cockpit/supply/euro/__init__.py
+++ emusim/cockpit/supply/euro/__init__.py
@@ -1,17 +1,10 @@
 from __future__ import annotations
 
 from .balance_sheet import BalanceSheet, BalanceSheetTimeline
-
 from .economic_actor import EconomicActor
-
 from .central_bank import CentralBank
-
 from .bank import Bank
-
 from .private_actor import PrivateActor
-
 from .euro_economy import EuroEconomy
-
 from .simple_data_generator import SimpleDataGenerator
-
 from .aggregate_simulator import AggregateSimulator
