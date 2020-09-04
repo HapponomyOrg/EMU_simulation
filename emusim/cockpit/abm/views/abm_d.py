b69b80d30542d8f68e373c06cac7bedbde9578d2
lansen.puttemans <8037dresT>
2020-09-04 09:31:24 +0200

    implementation of user management (including mailing), and user's model management
    Signed-off-by: lansen.puttemans <8037dresT>

--- emusim/cockpit/abm/views/abm_d.py
+++ emusim/cockpit/abm/views/abm_d.py
@@ -1,17 +1,9 @@
-import base64
-import os
-
-from flask import Blueprint, render_template, request, Response, make_response, send_file
+from flask import Blueprint, render_template, request
 from emusim.cockpit.abm.forms.abm_forms_d import ParameterForm
 from emusim.cockpit.abm.mesa.MoneyModel import *
 import pygal
 import numpy as np
 
-from reportlab.graphics import renderPDF
-from reportlab.pdfgen import canvas
-from svglib.svglib import svg2rlg
-
-from emusim.cockpit.abm.views.pdfreport import pdfreport
 
 abm_in_flask = Blueprint('abm_in_flask', __name__,
                          template_folder='../templates',
@@ -100,60 +92,4 @@ def moneymodel(parameter_form=None):
                            graph_data=graph_data,
                            render_graphs=render_graphs)
 
-
-#@abm_in_flask.route('/modelreport', methods=['GET', 'POST'])
-def get_report():
-    reportname = 'svg_on_canvas.pdf'
-    imagename = 'newfile.svg'
-    if os.path.exists(reportname): os.remove(reportname)
-    if os.path.exists(imagename): os.remove(imagename)
-    img = (graph_data[0])[0]
-    encoded = img.replace("data:image/svg+xml;charset=utf-8;base64,", "")
-    decoded = base64.b64decode(encoded)
-    with open(imagename, "wb") as f:f.write(decoded)
-    image_path = imagename
-    my_canvas = canvas.Canvas(reportname)
-    drawing = svg2rlg(image_path)
-    scaled_drawing = scale(drawing, scaling_factor=0.5)
-    renderPDF.draw(scaled_drawing, my_canvas, 0, 40)
-    my_canvas.drawString(50, 30, 'My SVG Image')
-    my_canvas.save()
-    #response = make_response(my_canvas.encode('latin-1'))
-    #response.headers.set('Content-Disposition', 'attachment', filename=reportname)
-    #response.headers.set('Content-Type', 'application/pdf')
-    return send_file(reportname, as_attachment=True)
-
-
-def scale(drawing, scaling_factor):
-    """
-    Scale a reportlab.graphics.shapes.Drawing()
-    object while maintaining the aspect ratio
-    """
-    scaling_x = scaling_factor
-    scaling_y = scaling_factor
-
-    drawing.width = drawing.minWidth() * scaling_x
-    drawing.height = drawing.height * scaling_y
-    drawing.scale(scaling_x, scaling_y)
-    return drawing
-
-def get_report_pdf2():
-    reportname = 'report'
-    pdf = pdfreport()
-    pdf.add_page()
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(40, 10, 'Hello World!')
-    for graphs in graph_data:
-        for graph in graphs:
-            print('joepie')
-            img = graph
-            pdf_path = '/tmp'
-            #pdf.image(graph, h=70, w=150, x=30, y=100, type="jpg")
-            pdf.sample_pdf(img, pdf_path)
-
-    # pdf.output(filename, 'F')
-    response = make_response(pdf.output(dest='S').encode('latin-1'))
-    response.headers.set('Content-Disposition', 'attachment', filename=reportname + '.pdf')
-    response.headers.set('Content-Type', 'application/pdf')
-    return response
 
