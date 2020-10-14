36825cb509c9e4a2550f16a592d0c452346e2384
stef <stef@thestandupway.org>
2020-10-14 08:38:08 +0200

    Initial version
    Signed-off-by: stef <stef@thestandupway.org>

---
+++ emusim/pytest/test_cycles.py
@@ -0,0 +1,11 @@
+from emusim.cockpit.utilities.cycles import Period, Interval
+
+
+def test_period_completion():
+    year: Period = Period(1, Interval.YEAR)
+    month = Period(1, Interval.MONTH)
+    week = Period(1, Interval.WEEK)
+
+    assert year.period_complete(Period.YEAR_DAYS - 1)
+    assert month.period_complete(Period.MONTH_DAYS - 1)
+    assert week.period_complete(Period.WEEK_DAYS - 1)
