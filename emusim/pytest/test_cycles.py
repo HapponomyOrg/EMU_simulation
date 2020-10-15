94826f7f5dd72bc0640fa0ad5766b761cd62636b
stef <stef@thestandupway.org>
2020-10-15 06:35:50 +0200

    Updated
    Signed-off-by: stef <stef@thestandupway.org>

--- emusim/pytest/test_cycles.py
+++ emusim/pytest/test_cycles.py
@@ -6,10 +6,6 @@ def test_period_completion():
     month = Period(1, Interval.MONTH)
     week = Period(1, Interval.WEEK)
 
-    assert year.period_complete(Period.YEAR_DAYS)
-    assert month.period_complete(Period.MONTH_DAYS)
-    assert week.period_complete(Period.WEEK_DAYS)
-
     assert year.period_complete(Period.YEAR_DAYS - 1)
     assert month.period_complete(Period.MONTH_DAYS - 1)
     assert week.period_complete(Period.WEEK_DAYS - 1)
